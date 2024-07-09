f3e2f86d367d3ea32c3d9a3dc2cb109a85728b49
sathishprofessionaltimesaver@gmail.com <sathishprofessionaltimesaver@gmail.com>
2024-07-09 02:09:55 +0000

    name change

---
+++ models/example/my_fourth_dbt_uniall.sql
@@ -0,0 +1,12 @@
+
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with tem_table as (
+   ( select * from DBT_NEW_MODEL.PUBLIC.COUNTRY)
+   union all 
+   (  select * from  {{ ref('my_third_emp_model') }}) 
+)
